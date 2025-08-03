3c847b53e188b68539480761292ba57c40c2f4d7
MissyCanada2019 <terri.bertin57@gmail.com>
2025-08-03 00:57:02 +0000

    commit

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
+    is_active = db.Column(db.Boolean, default=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
