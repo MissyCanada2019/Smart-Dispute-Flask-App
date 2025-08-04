bbb288a7aa462e97f7af260dbe6bfe56a35520f3
MissyCanada2019 <you@example.com>
2025-08-04 07:13:42 +0000

    Fix: correct import in models/__init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 from .user import User
 from .case import Case
-from .court_form import CourtForm
+from .court_form import FormTemplate, FormField, FormSubmission
 from .evidence import Evidence
 from .legal_journey import LegalJourney
 from .notification import Notification
